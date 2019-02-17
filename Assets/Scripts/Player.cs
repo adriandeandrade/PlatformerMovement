7dd25e052bdfdf1671334eee0acb5e9f1d703bdf
Adrian De Andrade <redfoxhint4@hotmail.com>
2019-02-16 21:43:23 -0500

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Controller2D))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpHeight = 4;
+    [SerializeField] private float timeToJumpApex = .4f;
+
+    float accelerationTimeAirborne = .2f;
+    float accelerationTimeGrounded = .3f;
+    float velocityXSmoothing;
+    float gravity;
+    float jumpVelocity;
+
+    Vector3 velocity;
+
+    Controller2D controller;
+
+
+    private void Awake()
+    {
+        controller = GetComponent<Controller2D>();
+    }
+
+    private void Start()
+    {
+        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+        Debug.Log("Gravity" + gravity + "Jump Velocity: " + jumpVelocity);
+    }
+
+    private void Update()
+    {
+        if (controller.collisions.above || controller.collisions.below)
+        {
+            velocity.y = 0;
+        }
+
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (Input.GetKeyDown(KeyCode.Space) && controller.collisions.below)
+        {
+            velocity.y = jumpVelocity;
+        }
+
+        float targetVelocityX = input.x * moveSpeed;
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
+        velocity.x = input.x * moveSpeed;
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+
+
+
+}
